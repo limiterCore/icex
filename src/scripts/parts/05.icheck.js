441fc47838c0e1c3a3ea1ee0252f3055a636506e
Alexandr4e <blacksbox@yandex.ru>
2018-01-23 12:53:34 +0700

    Add Export page(3 in 1)

--- src/scripts/parts/05.icheck.js
+++ src/scripts/parts/05.icheck.js
@@ -4,5 +4,8 @@
 (function($) {
 	$('.js-checkbox').iCheck({
 		checkboxClass: "checkbox-styled",
-	})
+	});
+	$('.js-checkbox-small').iCheck({
+		checkboxClass: "checkbox-styled checkbox-styled_small",
+	});
 })(jQuery);
