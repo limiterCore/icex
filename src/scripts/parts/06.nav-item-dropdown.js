3f28d309b3fad41a7584c65459185e2a59d90db5
Innokenty Saraev <Evgenia Filippova>
2018-01-24 11:03:23 +0700

    Fixes for mobile header dropdown

--- src/scripts/parts/06.nav-item-dropdown.js
+++ src/scripts/parts/06.nav-item-dropdown.js
@@ -6,9 +6,11 @@
 
 	$link.click(function() {
 		var $t = $(this),
-			$dropdown = $t.find('.js-header-nav-item-dropdown');
+			$parent = $t.closest('.js-header-nav-item-parent'),
+			$dropdown = $parent.find('.js-header-nav-item-dropdown');
 
 		$dropdown.toggleClass('header__nav-item-dropdown_open');
+		$dropdown.slideToggle(300);
 		$t.toggleClass('header__nav-item_open');
 	});
 })(jQuery);
