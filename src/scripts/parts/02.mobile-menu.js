b6fc8ec54d3b005ac4079945fada8645dc8dd2b7
Innokenty Saraev <Evgenia Filippova>
2018-01-24 11:57:59 +0700

    Foxes for mobile menu

--- src/scripts/parts/02.mobile-menu.js
+++ src/scripts/parts/02.mobile-menu.js
@@ -1,9 +1,11 @@
 (function() {
 	var $burger = $('#burger'),
-		$nav = $('#nav');
+		$nav = $('#nav'),
+		$header = $('#header');
 
 	$burger.click(function() {
 		$burger.toggleClass('header__burger_active');
 		$nav.slideToggle(300);
+		$header.toggleClass('mobile-menu-open');
 	});
 })();
